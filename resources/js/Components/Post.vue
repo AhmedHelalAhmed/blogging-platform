2da16abed8d9e3f159b66c6711d5b32241684934
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-01-26 07:14:57 +0200

    Enhance UI

--- resources/js/Components/Post.vue
+++ resources/js/Components/Post.vue
@@ -9,7 +9,9 @@ defineProps(['title', 'description', 'publishedAt']);
                 <h2 class="mb-3 uppercase border-black-600 font-bold shadow-sm pt-2 pb-2">
                     {{ title }}
                 </h2>
-                <pre class="break-words whitespace-pre">{{ description }}</pre>
+                <div class="break-normal whitespace-pre-wrap">
+                    <div>{{ description }}</div>
+                </div>
                 <div class="text-right">
                     <small class="font-bold text-gray-600">{{ publishedAt }}</small>
                 </div>
